4e5312252e34d70050bc897178aabefc6d622344
Chris Liaw <chrisliaw@antrapol.com>
2020-10-15 14:28:58 +0800

    Adding test suite

--- test/toolrack_test.rb
+++ test/toolrack_test.rb
@@ -1,16 +1,21 @@
-require "test_helper"
+require_relative "test_helper"
+
+require 'toolrack'
 
 class ToolrackTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil Antrapol::ToolRack::VERSION
   end
 
-  def test_pty
-    Antrapol::ToolRack::ProcessUtilsEngine.pty_exec(["ssh-keygen","-t","ed25519","-a","188","-o","-f","test.ssh","-C","chrisliaw@antrapol.com"]) do |ops, val|
-      p val
-      print val[:data].join
+  #def test_pty
+  #  Antrapol::ToolRack::ProcessUtilsEngine.pty_exec(["ssh-keygen","-t","ed25519","-a","188","-o","-f","test.ssh","-C","chrisliaw@antrapol.com"]) do |ops, val|
+  #    p val
+  #    print val[:data].join
+  #  end
+  #end
 
-    end
+  def test_basic
+    p Antrapol::ToolRack::ProcessUtilsEngine.exec("sudo apt update")
   end
 
 end
