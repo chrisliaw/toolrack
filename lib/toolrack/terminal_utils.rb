8900a16099406c78db426cc218b355c4723bbaf0
Chris <chris@antrapol.com>
2023-12-10 18:39:19 +0800

    Fix double quote in Mac OS error issue

--- lib/toolrack/terminal_utils.rb
+++ lib/toolrack/terminal_utils.rb
@@ -1,70 +1,73 @@
-
 require_relative 'runtime_utils'
 
 module Antrapol
   module ToolRack
-    
     class TerminalUtilsException < StandardError; end
 
     module TerminalUtils
-     
       def tu_new_terminal(terminal, cmd)
-
-        cmd = [cmd] if not cmd.is_a?(Array)
+        cmd = [cmd] unless cmd.is_a?(Array)
 
         case terminal
-        when "terminator"
-          `#{terminal} -x "#{cmd.join(" ")}"`
+        when 'terminator'
+          ToolRack.logger("termutils").debug "Command to be run in terminal '#{terminal}' : #{cmd.join(" ")}"
+          `#{terminal} -x "#{cmd.join(' ')}"`
 
-        when "gnome-terminal"
-          `#{terminal} -- bash -c "#{cmd.join(" ")}; exec bash"`
+        when 'gnome-terminal'
+          ToolRack.logger("termutils").debug "Command to be run in terminal '#{terminal}' : #{cmd.join(" ")}"
+          `#{terminal} -- bash -c "#{cmd.join(' ')}; exec bash"`
 
-        when "iTerm2"
+        when 'iTerm2'
+         
+          ToolRack.logger("termutils").debug "Command to be run in terminal '#{terminal}' : #{cmd.join(" ").gsub("\"","\\\"")}"
+
           `osascript -e \
-               'tell application "iTerm"
-               activate
+            'tell application "iTerm"
+            activate
 
-               create window with default profile
-               delay 0.5
+            create window with default profile
+            delay 0.5
 
-               set currentWindow to current window
+            set currentWindow to current window
 
-               tell current session of currentWindow
-               write text "#{cmd.join(" ")}"
-               end tell
+            tell current session of currentWindow
+              write text "#{cmd.join(' ').gsub("\"","\\\"")}"
+            end tell
 
-               end tell'
+            end tell'
           `
-        when "Terminal" 
+        when 'Terminal'
+          
+          ToolRack.logger("termutils").debug "Command to be run in terminal '#{terminal}' : #{cmd.join(" ").gsub("\"","\\\"")}"
+
           `osascript -e \
                'tell application "Terminal"
                activate
-               do script "#{cmd.join(" ")}"
+               do script "#{cmd.join(' ').gsub("\"","\\\"")}"
                end tell'
           `
 
         else
-          raise TerminalUtilsException, "Terminal '#{terminal}' not supported. Supported terminal are : #{tu.possible_terminal.join(", ")}" 
+          raise TerminalUtilsException,
+                "Terminal '#{terminal}' not supported. Supported terminal are : #{tu.possible_terminal.join(', ')}"
         end
-
       end
 
       def tu_possible_terminal
         avail = []
         if RuntimeUtils.on_linux?
-          possible = [ "gnome-terminal","konsole","tilix", "terminator" ]
+          possible = %w[gnome-terminal konsole tilix terminator]
           possible.each do |app|
-            avail << app if not File.which(app).nil?
+            avail << app unless File.which(app).nil?
           end
         elsif RuntimeUtils.on_windows?
-          avail << "cmd.exe"
+          avail << 'cmd.exe'
         elsif RuntimeUtils.on_mac?
-          avail << "Terminal"
-          avail << "iTerm2"
+          avail << 'Terminal'
+          avail << 'iTerm2'
         end
         avail
       end # tu_possible_terminal
-
     end
   end
 end
--- spec/terminal_utils_spec.rb
+++ spec/terminal_utils_spec.rb
@@ -1,16 +1,14 @@
-
 require_relative '../lib/toolrack/terminal_utils'
 
-RSpec.describe "Terminal Utils" do
-
+RSpec.describe 'Terminal Utils' do
   it 'spawn a terminal and run command in it' do
     class Driver
       include Antrapol::ToolRack::TerminalUtils
     end
 
     d = Driver.new
-    d.tu_new_terminal("terminator","ssh docean")
-    #d.tu_new_terminal("gnome-terminal","git status")
+    # d.tu_new_terminal("terminator","ssh docean")
+    d.tu_new_terminal('iTerm2', 'ssh docean')
+    # d.tu_new_terminal("gnome-terminal","git status")
   end
-
 end
